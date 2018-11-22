1b91998b55b95bddbfef6135751ebd8b89ae889a
Payam Nab <papampi@users.noreply.github.com>
2018-11-22 21:11:18 +0330

    Update net_check.sh

--- net_check.sh
+++ net_check.sh
@@ -14,58 +14,59 @@ no_ping=0
 
 while true
 do
-if ping -c1 "$router_ip"
-then
-  while ! nc -vzw1 $server_1  && !  nc -vzw1  $server_2
-  do
-    if (( ping_ok >= 10 ))
-    then
-      echo "$(date) - [WARNING] - Name resolution problem while ping is ok, waited 5 minutes, restarting router" | tee -a $DIR/net_check.log
-      sleep 5
-      #reboot router
-      bash $DIR/router-reboot.sh
-      dns_check=0
-      ping_ok=0
-      while ! ping -c1 "$router_ip"
-      do
-        echo "Router rebooted, waiting..."
-        sleep 30
-      done
-    fi
+  if ping -c1 "$router_ip"
+  then
+    while ! nc -vzw1 $server_1  && !  nc -vzw1  $server_2
+    do
+      if (( ping_ok >= 10 ))
+      then
+        echo "$(date) - [WARNING] - Name resolution problem while ping is ok, waited 5 minutes, restarting router" | tee -a $DIR/net_check.log
+        sleep 5
+        #reboot router
+        bash $DIR/router-reboot.sh
+        dns_check=0
+        ping_ok=0
+        while ! ping -c1 "$router_ip"
+        do
+          echo "Router rebooted, waiting..."
+          sleep 30
+        done
+      fi
 
-    if (( no_ping >= 20 ))
-    then
-      echo "$(date) - [WARNING] - Name resolution problem and NO Ping, waited 10 minutes, restarting router" | tee -a $DIR/net_check.log
-      sleep 5
-      #reboot router
-      bash $DIR/router-reboot.sh
-      dns_check=0
-      no_ping=0
-      while ! ping -c1 "$router_ip"
-      do
-        echo "Router rebooted, waiting..."
-        sleep 30
-      done
-    fi
+      if (( no_ping >= 20 ))
+      then
+        echo "$(date) - [WARNING] - Name resolution problem and NO Ping, waited 10 minutes, restarting router" | tee -a $DIR/net_check.log
+        sleep 5
+        #reboot router
+        bash $DIR/router-reboot.sh
+        dns_check=0
+        no_ping=0
+        while ! ping -c1 "$router_ip"
+        do
+          echo "Router rebooted, waiting..."
+          sleep 30
+        done
+      fi
 
-    echo "$(date) - [WARNING] - Internet Problem, checking ..." | tee -a $DIR/net_check.log
-    if ping -c1 "$ping_ip" # &> /dev/null
-    then
-      echo "$(date) - [WARNING] - Ping ok, but DNS problem, check again in 30 sec"  | tee -a $DIR/net_check.log
-      (( ping_ok++ ))
-      echo "DEBUG: Ping OK #$ping_ok"
-    else
-      echo "$(date) - [WARNING] - No Ping and DNS, network down, check again in 30 sec" | tee -a $DIR/net_check.log
-      (( no_ping++ ))
-      echo "DEBUG: No Ping #$no_ping"
+      echo "$(date) - [WARNING] - Internet Problem, checking ..." | tee -a $DIR/net_check.log
+      if ping -c1 "$ping_ip" # &> /dev/null
+      then
+        echo "$(date) - [WARNING] - Ping ok, but DNS problem, check again in 30 sec"  | tee -a $DIR/net_check.log
+        (( ping_ok++ ))
+        echo "DEBUG: Ping OK #$ping_ok"
+      else
+        echo "$(date) - [WARNING] - No Ping and DNS, network down, check again in 30 sec" | tee -a $DIR/net_check.log
+        (( no_ping++ ))
+        echo "DEBUG: No Ping #$no_ping"
+      fi
+      sleep 30
+      (( dns_check++ ))
+      echo "DEBUG: DNS check #$dns_check"
+
     fi
+    echo "All good, check in 30"
     sleep 30
-    (( dns_check++ ))
-    echo "DEBUG: DNS check #$dns_check"
-if
-sleep 30
-done
-
-  echo "All good, check in 30"
+  done
+  echo "Router Down, check in 30"
   sleep 30
 done
